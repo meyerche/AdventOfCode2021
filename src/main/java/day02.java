396d8e855b52f8fc1c633edb6e3d857af1dd7e50
meyerche <meyerale@oregonstate.edu>
2021-12-02 08:43:50 -0500

    day 2 - part 2

--- src/main/java/day02.java
+++ src/main/java/day02.java
@@ -5,10 +5,9 @@ import java.util.List;
 public class day02 {
     public static void main(String[] args) {
         List<String> data = loadInputFile.ReadFileString("day02_input.txt");
-        //data.stream().forEach(System.out::println);
 
         subPosition finalPos;
-        finalPos = data.stream().map(subPosition::new).reduce(new subPosition(0,0), day02::sum);
+        finalPos = data.stream().map(subPosition::new).reduce(new subPosition(0,0,0), day02::move);
 
         System.out.println(finalPos.getDepth() * finalPos.getHorizontalPos() );
     }
@@ -18,29 +17,46 @@ public class day02 {
         return pos1;
     }
 
+    public static subPosition move(subPosition pos1, subPosition pos2) {
+        pos1.movePos(pos2);
+        return pos1;
+    }
 }
 
 class subPosition {
     private Integer depth;
     private Integer horizontalPos;
+    private Integer aim;
 
     public subPosition(String pos) {
         this.depth = 0;
         this.horizontalPos = 0;
+        this.aim = 0;
 
         int dist = Integer.parseInt(String.valueOf(pos.charAt(pos.length() - 1)));
 
         if (pos.charAt(0) == 'u') {
-            this.depth -= dist;
+            //this.depth -= dist;
+            this.aim -= dist;
         } else if (pos.charAt(0) == 'd') {
-            this.depth += dist;
+            //this.depth += dist;
+            this.aim += dist;
         } else if (pos.charAt(0) == 'f') {
             this.horizontalPos += dist;
         }
     }
-    public subPosition(Integer d, Integer h) {
-        depth = d;
-        horizontalPos = h;
+
+    public subPosition(Integer d, Integer h, Integer a) {
+        this.depth = d;
+        this.horizontalPos = h;
+        this.aim = a;
+    }
+
+    public subPosition() {
+        this.depth = 0;
+        this.horizontalPos = 0;
+        this.aim = 0;
+
     }
 
     public Integer getDepth() {
@@ -62,5 +78,13 @@ class subPosition {
     public void addPos(subPosition pos) {
         this.depth += pos.depth;
         this.horizontalPos += pos.horizontalPos;
+    }
+
+    public void movePos(subPosition pos) {
+        if (pos.horizontalPos != 0){
+            this.depth += (this.aim * pos.horizontalPos);
+            this.horizontalPos += pos.horizontalPos;
+        }
+        this.aim += pos.aim;
     }
 }
