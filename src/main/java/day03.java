434c1f046890630232371518ae055040f44a36b7
meyerche <meyerale@oregonstate.edu>
2021-12-03 13:17:28 -0500

    day 3 - part 2

--- src/main/java/day03.java
+++ src/main/java/day03.java
@@ -1,11 +1,9 @@
 import shared.loadInputFile;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class day03 {
     public static void main(String[] args) {
@@ -18,7 +16,38 @@ public class day03 {
         Double gamma = GetGamma(digitCount, data.size());
         Double epsilon = GetEpsilon(digitCount, data.size());
 
-        System.out.println(gamma * epsilon);
+        Integer oxygen = filterOxygen(data.stream(), digitCount, 0);
+        Integer co2 = filterC02(data.stream(), digitCount, 0);
+
+        System.out.println(gamma + " " + epsilon + " " + gamma * epsilon);
+        System.out.println(oxygen);
+        System.out.println(co2);
+        System.out.println(oxygen * co2);
+    }
+
+    private static Integer filterC02(Stream<String> stream, List<Integer> digitCount, int pos) {
+        List<String> lst = stream.collect(Collectors.toList());
+
+        if (lst.size() == 1) {
+            return lst.stream().map(x -> Integer.parseInt(x,2)).reduce(0, (x,y) -> y);
+        }
+
+        int filter = digitCount.get(pos) < 500 ? 1 : 0;
+
+        return filterOxygen(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), digitCount, pos + 1);
+    }
+
+    private static Integer filterOxygen(Stream<String> stream, List<Integer> digitCount, int pos) {
+        List<String> lst = stream.collect(Collectors.toList());
+
+        if (lst.size() == 1) {
+            return Integer.parseInt(lst.get(0), 2);
+            //return lst.stream().map(x -> Integer.parseInt(x,2)).reduce(0, (x,y) -> y);
+        }
+
+        int filter = digitCount.get(pos) < 500 ? 0 : 1;
+
+        return filterOxygen(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), digitCount, pos + 1);
     }
 
     private static Double GetEpsilon(List<Integer> digitCount, int size) {
