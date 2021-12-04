a628b82c9dc61b37425d6111152ce83552fde480
Meyer, Alex <ameyer2@paychex.com>
2021-12-04 07:21:38 -0500

    day 3 - part 2

--- src/main/java/day03.java
+++ src/main/java/day03.java
@@ -9,15 +9,14 @@ public class day03 {
     public static void main(String[] args) {
         List<String> data = loadInputFile.ReadFileString("day03_input.txt");
 
-        List<Integer> digitCount = data.stream()
-                .map(day03::MapToIntList)
-                .reduce(new ArrayList<>(Collections.nCopies(12, 0)), day03::SumList);
+
+        List<Integer> digitCount = GetMostCommon(data);
 
         Double gamma = GetGamma(digitCount, data.size());
         Double epsilon = GetEpsilon(digitCount, data.size());
 
-        Integer oxygen = filterOxygen(data.stream(), digitCount, 0);
-        Integer co2 = filterC02(data.stream(), digitCount, 0);
+        Integer oxygen = filterOxygen(data.stream(), 0);
+        Integer co2 = filterC02(data.stream(), 0);
 
         System.out.println(gamma + " " + epsilon + " " + gamma * epsilon);
         System.out.println(oxygen);
@@ -25,19 +24,29 @@ public class day03 {
         System.out.println(oxygen * co2);
     }
 
-    private static Integer filterC02(Stream<String> stream, List<Integer> digitCount, int pos) {
+    private static List<Integer> GetMostCommon(List<String> data) {
+        return data.stream()
+                .map(day03::MapToIntList)
+                .reduce(new ArrayList<>(Collections.nCopies(12, 0)), day03::SumList);
+    }
+
+    private static Integer filterC02(Stream<String> stream, int pos) {
         List<String> lst = stream.collect(Collectors.toList());
 
         if (lst.size() == 1) {
-            return lst.stream().map(x -> Integer.parseInt(x,2)).reduce(0, (x,y) -> y);
+            return Integer.parseInt(lst.get(0), 2);
         }
 
-        int filter = digitCount.get(pos) < 500 ? 1 : 0;
+        List<Integer> digitCount = GetMostCommon(lst);
 
-        return filterOxygen(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), digitCount, pos + 1);
+        int filter;
+//        filter = digitCount.get(pos) >= Math.ceil(lst.size()/2.0) ? 0 : 1;
+        filter = digitCount.get(pos) > lst.size()/2.0 ? 0 : 1;
+
+        return filterC02(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), pos + 1);
     }
 
-    private static Integer filterOxygen(Stream<String> stream, List<Integer> digitCount, int pos) {
+    private static Integer filterOxygen(Stream<String> stream, int pos) {
         List<String> lst = stream.collect(Collectors.toList());
 
         if (lst.size() == 1) {
@@ -45,9 +54,12 @@ public class day03 {
             //return lst.stream().map(x -> Integer.parseInt(x,2)).reduce(0, (x,y) -> y);
         }
 
-        int filter = digitCount.get(pos) < 500 ? 0 : 1;
+        List<Integer> digitCount = GetMostCommon(lst);
 
-        return filterOxygen(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), digitCount, pos + 1);
+        //int filter = digitCount.get(pos) >= Math.ceil(lst.size()/2.0) ? 1 : 0;
+        int filter = digitCount.get(pos) > lst.size()/2.0 ? 1 : 0;
+
+        return filterOxygen(lst.stream().filter(s -> s.charAt(pos) % 48 == filter), pos + 1);
     }
 
     private static Double GetEpsilon(List<Integer> digitCount, int size) {
