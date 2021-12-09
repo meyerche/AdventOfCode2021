9bea5f1cef7d5d0ee70555d69c05b56fae730d9b
Meyer, Alex <ameyer2@paychex.com>
2021-12-09 14:54:35 -0500

    day 9 - part 2 done

--- src/main/java/day09.java
+++ src/main/java/day09.java
@@ -10,6 +10,81 @@ public class day09 {
                         loadInputFile.class.getClassLoader().getResourceAsStream("day09_input.txt")
                 ));
 
+        //Part1(scanner);
+
+        Part2(scanner);
+
+    }
+
+    private static void Part2(Scanner scanner) {
+        //create hashmap of all the points
+        List<Integer> points = new ArrayList<>();
+        int idx;
+        int len = 0;
+
+        while(scanner.hasNext()) {
+            String[] newline = scanner.nextLine().split("");
+            len = newline.length;
+            IntStream.range(0,newline.length).forEach(i -> points.add(Integer.parseInt(newline[i])));
+        }
+
+        HashSet<Integer> pointsChecked = new HashSet<>();
+        List<Integer> basinSizes = new ArrayList<>();
+
+        idx = 0;
+        while (idx < points.size()) {
+
+            if (!pointsChecked.contains(idx) && points.get(idx) != 9) {
+                basinSizes.add(FindBasin(idx, len, points, pointsChecked)) ;
+            }
+
+            idx++;
+        }
+
+        System.out.println(basinSizes.stream().sorted(Comparator.reverseOrder()).limit(3).reduce(1,(x,y)->x*y));
+    }
+
+    private static Integer FindBasin(int idx, int lineLength, List<Integer> points, HashSet<Integer> pointsChecked) {
+        int count = 0;
+        Stack<Integer> pointsToCheck = new Stack<>();
+        pointsToCheck.push(idx);
+
+        int i, up, right, down, left;
+
+        List<Integer> pointsInBasin = new ArrayList<>();
+
+        while (!pointsToCheck.isEmpty()) {
+            i = pointsToCheck.remove(0);
+            up = i - lineLength;
+            right = i % lineLength == lineLength - 1 ? Integer.MAX_VALUE : i + 1;
+            down = i + lineLength;
+            left = i % lineLength == 0 ? -1 : i - 1;
+
+            //check neighbors
+            if (!pointsChecked.contains(i)) {
+                pointsChecked.add(i);
+                count++;
+                pointsInBasin.add(i);
+
+                if (up >= 0 && points.get(up) < 9 && !pointsChecked.contains(up)) {
+                    pointsToCheck.push(up);
+                }
+                if (right < points.size() && points.get(right) < 9 && !pointsChecked.contains(right)) {
+                    pointsToCheck.push(right);
+                }
+                if (down < points.size() && points.get(down) < 9 && !pointsChecked.contains(down)) {
+                    pointsToCheck.push(down);
+                }
+                if (left >= 0 && points.get(left) < 9 && !pointsChecked.contains(left)) {
+                    pointsToCheck.push(left);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void Part1(Scanner scanner) {
         List<Integer> lowPoints = new ArrayList<>();
         String prev = "";
         String cur = "";
@@ -57,4 +132,5 @@ public class day09 {
 //9144 (too high)
 //1774 too high
 //1327 too high
-//
+
+//6744295 too high
