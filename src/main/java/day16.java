13f147819e0252b184d2654563383e3676da9f56
meyerche <meyerale@oregonstate.edu>
2021-12-17 01:31:10 -0500

    day 16 - not working - abandon

--- src/main/java/day16.java
+++ src/main/java/day16.java
@@ -7,38 +7,40 @@ import java.util.stream.IntStream;
 
 public class day16 {
     private static final String[] BIN_FROM_HEX = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
+    private static int versionSum;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(day16.class.getClassLoader().getResourceAsStream("day16_input.txt"));
+        Scanner scanner = new Scanner(day16.class.getClassLoader().getResourceAsStream("day16_input_test.txt"));
         String data = "";
 
         //only one line
         if (scanner.hasNext()) data = scanner.nextLine();
 
-        ProcessCode(data.split(""));
+        Deque<String> hexCode = new ArrayDeque<>(Arrays.asList(data.split("")));
 
+        versionSum = 0;
+
+        ProcessCode(hexCode, new ArrayDeque<>(), -1);
+
+        System.out.println(versionSum);
     }
 
-    private static void ProcessCode(String[] arr) {
-        Deque<String> hexCode = new ArrayDeque<>(Arrays.asList(arr));
-        Deque<String> binCode = new ArrayDeque<>(); //holds leftover binary digits
+    private static void ProcessCode(Deque<String> hexCode, Deque<String> binCode, int packetCount) {
+        while (packetCount != 0) {
+            if (hexCode.isEmpty() || RestIsZero(hexCode,binCode)) return;
 
-        int version;
-        int versionSum = 0;
-        int packetTypeID;
-        int literalValue;
-        String lengthTypeID;
-
-        while (!hexCode.isEmpty()) {
             //start each sequence with the version code
-            version = GetVersion(hexCode.pollFirst(), binCode);
-            versionSum += version;
+            versionSum += GetVersion(hexCode.pollFirst(), binCode);
+            System.out.println(versionSum);
+            int literalValue;
 
             //next add another digit to get the packet type ID
+            int packetTypeID;
             packetTypeID = GetPacketTypeID(hexCode.pollFirst(), binCode);
 
             if (packetTypeID == 4) {
                 literalValue = ReadLiteralValue(hexCode, binCode);
             } else {
+                if (binCode.isEmpty()) AddHexDigitToCode(hexCode.pollFirst(), binCode);
                 if (binCode.pollFirst().equals("0")) {
                     //Process sub-packets by bit length
                     ProcessSubPacketsByBitLength(hexCode, binCode);
@@ -47,9 +49,12 @@ public class day16 {
                     ProcessSubPacketsByCount(hexCode, binCode);
                 };
             }
+
+            packetCount -= 1;
         }
 
-        System.out.println(versionSum);
+
+        //System.out.println(versionSum);
     }
 
     private static void ProcessSubPacketsByCount(Deque<String> hexCode, Deque<String> binCode) {
@@ -59,14 +64,11 @@ public class day16 {
                 2
         );
 
-        while (binCode.size() < subPacketCount * 11) AddHexDigitToCode(hexCode.pollFirst(), binCode);
-        while (hexCode.peekFirst().equals("0")) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+        ProcessCode(hexCode, binCode, subPacketCount);
+        while (!hexCode.isEmpty() && hexCode.peekFirst().equals("0")) hexCode.pollFirst();
 
-        binCode.clear();  //do nothing for now
+        binCode.clear();  //binCode should be clear anyway when done with sub packets
         //maybe for part 2 will need to do something with each sub-packet
-//        for (int i = 0; i < subPacketCount; i++) {
-//            while (binCode.size() < 11) AddHexDigitToCode(hexCode.pollFirst(), binCode);
-//        }
     }
 
     private static void ProcessSubPacketsByBitLength(Deque<String> hexCode, Deque<String> binCode) {
@@ -76,8 +78,16 @@ public class day16 {
                 2
         );
 
-        while (binCode.size() < subPacketLength) AddHexDigitToCode(hexCode.pollFirst(), binCode);
-        while (!hexCode.isEmpty() && hexCode.peekFirst().equals("0")) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+        Deque<String> subPacketCode = new ArrayDeque<>();
+        while (!hexCode.isEmpty() && binCode.size() + 4*subPacketCode.size() < subPacketLength) {
+            subPacketCode.addLast(hexCode.pollFirst());
+        }
+        while (!hexCode.isEmpty() && hexCode.peekFirst().equals("0")) {
+            subPacketCode.addLast(hexCode.pollFirst());
+            //hexCode.pollFirst();
+        }
+
+        ProcessCode(subPacketCode, binCode, -1); //-1 means don't process based on packet count
 
         //do something with sub-packet???
 
@@ -102,17 +112,19 @@ public class day16 {
                     .collect(Collectors.joining("")));
         } while (notLastGroup.equals("1"));
 
-        binCode.clear();
+        //binCode.clear();
 
         return Integer.parseInt(binLiteralValue.toString());
     }
 
     private static void AddHexDigitToCode(String hexDigit, Deque<String> binCode) {
+        if (hexDigit == null) return;
         Arrays.stream(BIN_FROM_HEX[Integer.parseInt(hexDigit,16)].split(""))
                 .forEach(d -> binCode.addLast(d));
     }
 
     private static int GetPacketTypeID(String hexDigit, Deque<String> binCode) {
+        //if (binCode.size() < 3) AddHexDigitToCode(hexDigit, binCode);
         AddHexDigitToCode(hexDigit, binCode);
 
         //read first three digits from binary code stream to get packet type ID
@@ -122,10 +134,16 @@ public class day16 {
     }
 
     private static int GetVersion(String hexDigit, Deque<String> binCode) {
-        int decValue = Integer.parseInt(hexDigit,16);
-        binCode.addLast(Integer.toBinaryString(decValue & 1));
+        //if (binCode.size() < 3) AddHexDigitToCode(hexDigit, binCode);
+        AddHexDigitToCode(hexDigit, binCode);
+        return Integer.parseInt(
+                IntStream.range(0,3).mapToObj(i -> binCode.pollFirst()).collect(Collectors.joining("")),
+                2
+        );
+    }
 
-        return decValue >> 1; //bit shift to the right by 1
+    private static boolean RestIsZero(Deque<String> hexCode, Deque<String> binCode) {
+        return (binCode.stream().allMatch(n -> n.equals("0")) && hexCode.stream().allMatch(h -> h.equals("0")));
     }
 
 }
