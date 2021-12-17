bb5562c1206a3843ba93d8ce4a60b6c88d98713c
Meyer, Alex <ameyer2@paychex.com>
2021-12-16 21:21:15 -0500

    day 16 - part 1 still a work in progress

--- src/main/java/day16.java
+++ src/main/java/day16.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class day16 {
+    private static final String[] BIN_FROM_HEX = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
     public static void main(String[] args) {
         Scanner scanner = new Scanner(day16.class.getClassLoader().getResourceAsStream("day16_input.txt"));
         String data = "";
@@ -30,6 +31,7 @@ public class day16 {
         while (!hexCode.isEmpty()) {
             //start each sequence with the version code
             version = GetVersion(hexCode.pollFirst(), binCode);
+            versionSum += version;
 
             //next add another digit to get the packet type ID
             packetTypeID = GetPacketTypeID(hexCode.pollFirst(), binCode);
@@ -42,9 +44,29 @@ public class day16 {
                     ProcessSubPacketsByBitLength(hexCode, binCode);
                 } else {
                     //Process sub-packets by number of sub-packets
+                    ProcessSubPacketsByCount(hexCode, binCode);
                 };
             }
         }
+
+        System.out.println(versionSum);
+    }
+
+    private static void ProcessSubPacketsByCount(Deque<String> hexCode, Deque<String> binCode) {
+        while (binCode.size() < 11) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+        int subPacketCount = Integer.parseInt(
+                IntStream.range(0,11).mapToObj(d -> binCode.pollFirst()).collect(Collectors.joining("")),
+                2
+        );
+
+        while (binCode.size() < subPacketCount * 11) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+        while (hexCode.peekFirst().equals("0")) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+
+        binCode.clear();  //do nothing for now
+        //maybe for part 2 will need to do something with each sub-packet
+//        for (int i = 0; i < subPacketCount; i++) {
+//            while (binCode.size() < 11) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+//        }
     }
 
     private static void ProcessSubPacketsByBitLength(Deque<String> hexCode, Deque<String> binCode) {
@@ -55,7 +77,7 @@ public class day16 {
         );
 
         while (binCode.size() < subPacketLength) AddHexDigitToCode(hexCode.pollFirst(), binCode);
-        while (hexCode.peekFirst().equals("0")) AddHexDigitToCode(hexCode.pollFirst(), binCode);
+        while (!hexCode.isEmpty() && hexCode.peekFirst().equals("0")) AddHexDigitToCode(hexCode.pollFirst(), binCode);
 
         //do something with sub-packet???
 
@@ -86,15 +108,15 @@ public class day16 {
     }
 
     private static void AddHexDigitToCode(String hexDigit, Deque<String> binCode) {
-        Arrays.stream(Integer.toBinaryString(Integer.parseInt(hexDigit,16)).split(""))
+        Arrays.stream(BIN_FROM_HEX[Integer.parseInt(hexDigit,16)].split(""))
                 .forEach(d -> binCode.addLast(d));
     }
 
     private static int GetPacketTypeID(String hexDigit, Deque<String> binCode) {
-        binCode.addLast(Integer.toBinaryString(Integer.parseInt(hexDigit)));
+        AddHexDigitToCode(hexDigit, binCode);
 
         //read first three digits from binary code stream to get packet type ID
-        String binPacket = IntStream.rangeClosed(0,3).mapToObj(i -> binCode.removeFirst()).collect(Collectors.joining(""));
+        String binPacket = IntStream.range(0,3).mapToObj(i -> binCode.pollFirst()).collect(Collectors.joining(""));
 
         return Integer.parseInt(binPacket,2);
     }
@@ -105,4 +127,5 @@ public class day16 {
 
         return decValue >> 1; //bit shift to the right by 1
     }
+
 }
